75921500e692c90d98d0344740daed58611fef8d
canantu <tunc.canan84@gmail.com>
2024-04-04 02:49:52 +0200

    [u] Bank Simulation App

--- src/main/java/com/cydeo/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TransactionServiceImpl.java
@@ -1,8 +1,12 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.enums.AccountType;
+import com.cydeo.exception.AccountOwnershipException;
+import com.cydeo.exception.BalanceNotSufficientException;
 import com.cydeo.model.Account;
 import com.cydeo.model.Transaction;
 import com.cydeo.repository.AccountRepository;
+import com.cydeo.repository.TransactionRepository;
 import com.cydeo.service.TransactionService;
 import com.cydeo.exception.BadRequestException;
 import org.springframework.stereotype.Component;
@@ -14,12 +18,15 @@ import java.util.UUID;
 
 @Component
 public class TransactionServiceImpl implements TransactionService {
+    private final AccountRepository accountRepository;
+    private final TransactionRepository transactionRepository;
 
-    public TransactionServiceImpl(AccountRepository accountRepository) {
+    public TransactionServiceImpl(AccountRepository accountRepository, TransactionRepository transactionRepository) {
         this.accountRepository = accountRepository;
+        this.transactionRepository = transactionRepository;
     }
 
-    private final AccountRepository accountRepository;
+
     @Override
     public Transaction makeTransfer(Account sender, Account receiver, BigDecimal amount, Date dateCreated, String message) {
         /*
@@ -30,9 +37,48 @@ public class TransactionServiceImpl implements TransactionService {
          */
 
         validateAccount(sender, receiver);
+        checkAccountOwnership(sender, receiver);
+        executeBalanceAndUpdateIfRequired(amount, sender, receiver);
+
+        /*
+            After all validations are completed and transfer is done, we need Transaction object and save/return it.
+         */
+
+        Transaction transaction = Transaction.builder().amount(amount).sender(sender.getId()).receiver(receiver.getId())
+                .message(message).createDate(dateCreated).build();
+
+        // save into db and return
+        return transactionRepository.save(transaction);
+
+    }
+
+    private void executeBalanceAndUpdateIfRequired(BigDecimal amount, Account sender, Account receiver) {
+
+        if (checkSenderBalance(amount, sender)){
+            //update sender and receiver
+            sender.setBalance(sender.getBalance().subtract(amount));
+            receiver.setBalance(receiver.getBalance().add(amount));
+        }else{
+            throw new BalanceNotSufficientException("Balance is not enough to complete the transfer");
+        }
+    }
+
+    private boolean checkSenderBalance(BigDecimal amount, Account sender) {
+        return sender.getBalance().compareTo(amount) >= 0;
+    }
 
 
-        return null;
+    private void checkAccountOwnership(Account sender, Account receiver) {
+
+        /*
+            write an if statement that checks if one of the accounts is saving,
+            and user of the saver and checking are not same, throw AccountOwnershipException
+         */
+
+        if ( (sender.getAccountType()== AccountType.SAVING || receiver.getAccountType().equals(AccountType.SAVING))
+                && !sender.getUserId().equals(receiver.getUserId()) ){
+            throw new AccountOwnershipException("Transfer from a saving account can not be done to a different user's account.");
+        }
     }
 
     private void validateAccount(Account sender, Account receiver) {
@@ -52,6 +98,7 @@ public class TransactionServiceImpl implements TransactionService {
 
         findAccountById(sender.getId());
         findAccountById(receiver.getId());
+
 
 
     }
